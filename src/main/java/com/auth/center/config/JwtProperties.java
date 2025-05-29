b51b23b7423ba3236dad3f26712f209a5ddb842c
zhoujianbo <zhoujianbopro@163.com>
2025-05-29 15:34:40 +0800

    update

--- src/main/java/com/auth/center/config/JwtProperties.java
+++ src/main/java/com/auth/center/config/JwtProperties.java
@@ -2,6 +2,7 @@ package com.auth.center.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @Version: 1.0
  * @Date: 2025/5/14 17:13
  **/
+@RefreshScope
 @Data
 @Configuration
 public class JwtProperties {
--- src/main/java/com/auth/center/config/ShiroProperties.java
+++ src/main/java/com/auth/center/config/ShiroProperties.java
@@ -2,6 +2,7 @@ package com.auth.center.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @Version: 1.0
  * @Date: 2025/5/15 14:00
  **/
+@RefreshScope
 @Data
 @Configuration
 public class ShiroProperties {
